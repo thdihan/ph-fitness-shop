28fecd1f9bf04db1909eb4b1dc930391ab856de5
thdihan <thdihan@outlook.com>
2024-08-10 14:01:04 +0600

    Footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,52 +2,15 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 import { navItems } from "../Navbar/navItems";
 import { Link } from "react-router-dom";
+import {
+    ImFacebook2,
+    ImInstagram,
+    ImLinkedin2,
+    ImMail2,
+    ImTwitter,
+} from "react-icons/im";
+import { HashLink } from "react-router-hash-link";
 
-const navigation = [
-    { name: "Home", href: "#", current: true, subMenu: [] },
-    { name: "About", href: "#", current: false, subMenu: [] },
-    {
-        name: "Key Stage 1",
-        href: "#",
-        current: false,
-        subMenu: [
-            { name: "Maths", href: "#", current: false },
-            { name: "English", href: "#", current: false },
-            { name: "Science", href: "#", current: false },
-        ],
-    },
-    {
-        name: "Key Stage 2",
-        href: "#",
-        current: false,
-        subMenu: [
-            { name: "Maths", href: "#", current: false },
-            { name: "English", href: "#", current: false },
-            { name: "Science", href: "#", current: false },
-        ],
-    },
-    {
-        name: "AS/A Level",
-        href: "#",
-        current: false,
-        subMenu: [
-            { name: "Maths", href: "#", current: false },
-            { name: "English", href: "#", current: false },
-            { name: "Science", href: "#", current: false },
-        ],
-    },
-    {
-        name: "GSCE/IGCSE",
-        href: "#",
-        current: false,
-        subMenu: [
-            { name: "Maths", href: "#", current: false },
-            { name: "English", href: "#", current: false },
-            { name: "Science", href: "#", current: false },
-        ],
-    },
-    { name: "Contact", href: "#", current: false },
-];
 const Footer = () => {
     const [value, setValue] = useState(new Date());
     return (
@@ -64,19 +27,46 @@ const Footer = () => {
                     </address>
                 </div>
 
-                <div className="flex flex-1 justify-center">
-                    <ul className="text-center">
+                <div className="flex flex-1 justify-center space-x-8 items-center">
+                    <ul className="text-left">
                         {navItems.map((nav, index) => (
                             <li key={index}>
-                                <Link
+                                {nav.path.startsWith("/#") ? (
+                                    <HashLink
+                                        smooth
+                                        to={nav?.path}
+                                        className="block py-1 text-white"
+                                    >
+                                        {nav.name}
+                                    </HashLink>
+                                ) : (
+                                    <Link
+                                        to={nav?.path}
+                                        className="block py-1 text-white"
+                                    >
+                                        {nav.name}
+                                    </Link>
+                                )}
+                                {/* <Link
                                     to={nav?.path}
                                     className="block py-1 text-white"
                                 >
                                     {nav.name}
-                                </Link>
+                                </Link> */}
                             </li>
                         ))}
                     </ul>
+
+                    <div className="text-white space-y-2">
+                        <h2>Connect with us</h2>
+                        <div className="text-white flex text-xl space-x-2">
+                            <ImFacebook2 />
+                            <ImTwitter />
+                            <ImMail2 />
+                            <ImInstagram />
+                            <ImLinkedin2 />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="flex-1">
