ce23c4f74845ea5d14250034a1edfd961c0ae396
thdihan <thdihan@outlook.com>
2024-08-10 07:04:06 +0600

    Product card fixed

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -3,26 +3,29 @@ import { Button } from "antd";
 import { TProduct } from "../../types";
 const ProductCard = ({ product }: { product: TProduct }) => {
     return (
-        <div className="bg-white px-4 py-6 min-h-[300px] space-y-8 ">
+        <div className="bg-white px-4 py-6 min-h-[300px] space-y-8 flex flex-col justify-between border-2 border-r-0 last:border-r-2">
             <div className="flex justify-center">
                 <img src={product?.image} alt="" className="w-[150px]" />
             </div>
 
-            <div className="space-y-1">
-                <p className="text-sm text-gray-600">{product?.category}</p>
+            <div className="">
+                <div className="mb-4">
+                    <p className="text-sm text-gray-600">{product?.category}</p>
+                    <Link to={`/products/${product._id}`}>
+                        <h2 className="text-lg font-semibold text-left hover:text-[#FF5252] transition-all duration-300">
+                            {product?.name}
+                        </h2>
+                    </Link>
+                    <p>Stock: {product?.stock}</p>
+                    <p className="text-lg text-[#FF5252] font-semibold">
+                        ${product?.price}
+                    </p>
+                </div>
+
                 <Link to={`/products/${product._id}`}>
-                    <h2 className="text-lg font-semibold text-left hover:text-[#FF5252] transition-all duration-300">
-                        {product?.name}
-                    </h2>
-                </Link>
-                <p>Stock: {product?.stock}</p>
-                <p className="text-lg text-[#FF5252] font-semibold">
-                    ${product?.price}
-                </p>
-                <Link to={`/products/${product._id}`}>
-                    <Button className="w-full bg-[#FF5252] text-white font-semibold">
+                    <button className="w-full p-1 rounded-lg border-2 bg-[#FF5252] border-[#FF5252] hover:bg-[#FF7875] transition-all duration-200 text-white font-medium text-sm">
                         View Details
-                    </Button>
+                    </button>
                 </Link>
             </div>
         </div>
--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -2,7 +2,8 @@ import ProductCard from "./ProductCard";
 
 const ProductList = ({ products }) => {
     return (
-        <div className="my-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 border-2 border-grey-100 md:divide-x-2 divide-y-2 ">
+        // <div className="my-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 border-2 border-grey-100 md:divide-x-2 divide-y-2 ">
+        <div className="my-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 ">
             {products?.map((product) => (
                 <ProductCard key={product._id} product={product} />
             ))}
